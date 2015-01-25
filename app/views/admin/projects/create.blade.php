9e97604d949a38d0b2dd9a3ea4583843800ea884
jmramos02 <ramosjosemari@gmail.com>
2015-01-25 22:13:16 +0800

    Wakokok

--- app/views/admin/projects/create.blade.php
+++ app/views/admin/projects/create.blade.php
@@ -77,59 +77,6 @@
     </div>
 
 </div>
-<!--
-<div class="row" id="projects">
-    <div class="medium-10 large-centered column personal-info-container">
-        <h4 class="view-header"><i class="fa fa-building"></i> Project Collaborators</h4>
-
-        <div class="view-box">
-
-            <div class="medium-5 column search-box-container no-padding">
-
-                <div class="medium-1 column no-padding">
-                    <i class="fa fa-search"> </i>   
-                </div>
-            
-                <div class="medium-11 column">
-                    {{Form::text('search-collaborators','', array('class' => 'search-box'));}}    
-                </div>
-            </div>
-            
-            <div class="medium-7 column create-new-container no-padding">
-                <a href="{{URL::to('admin/projects/create')}}" class="small button create-collab"><i class="fa fa-plus"></i> Add Collaborator</a>
-            </div>
-
-            <table>
-                  <thead>
-                    <tr>
-                      <th>Position</th>
-                      <th>Last Name</th>
-                      <th>First Name</th>
-                      <th>Middle Name</th>
-                      <th>Contact No.</th>
-                      <th colspan="2">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Position</td>
-                      <td>Last_Name</td>
-                      <td>First_name</td>
-                      <td>Middle_name</td>
-                      <td>Contact_No</td>
-                      <td>
-                        <a href="{{URL::to('')}}">
-                                <i class="fa fa-pencil fa-2x"></i>
-                          </a>
-                      </td>
-                    </tr>
-                  </tbody>
-            </table>
-            
-        </div>
-    </div>
-</div>
-!-->
 <div class="row">
     <div class="medium-10 large-centered column account-info-container"> 
         {{Form::submit('Create New Project', array('class'=>'right button submit'))}}
