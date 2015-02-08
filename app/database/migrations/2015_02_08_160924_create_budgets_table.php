fbd64ddf69f37c5307cb84825c1438b2ffa6dbd6
jaotors <turingan.joshua@gmail.com>
2015-02-09 00:16:23 +0800

    added budget in db

---
+++ app/database/migrations/2015_02_08_160924_create_budgets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBudgetsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('budgets', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('project_id')->unsigned();
+			$table->decimal('amount',16,2);
+			$table->string('remarks');
+			$table->string('description');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('budgets');
+	}
+
+}
---
+++ app/views/budgets/create.blade.php
@@ -0,0 +1,1 @@
+Q:\WebProjects\laravelapp\public_html\silver\app/views/budgets/create.blade.php
