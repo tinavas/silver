d45c9634f52d85d6feef41acee75735ab6ba7d6f
jaotors <turingan.joshua@gmail.com>
2015-02-19 23:56:30 +0800

    fix the view

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -60,5 +60,16 @@ class UsersTableSeeder extends Seeder {
 			'activated' => true
 		]);
 		$user->addGroup($architectGroup);
+
+		$user = Sentry::createUser([
+			"email" => 'turingan.joshua@gmail.com',
+			'password' => 'test',
+			'first_name' => 'Joshua',
+			'last_name' => 'Turingan',
+			'address' => '983 metom st. brgy. comm. q.c',
+			'contact_number' => '+639054005755',
+			'activated' => true
+		]);
+		$user->addGroup($architectGroup);
 	}
 }
