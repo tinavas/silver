898269bb2eed145dc5b0905b64437234db232f6b
jaotors <turingan.joshua@gmail.com>
2015-04-12 20:45:38 +0800

    added OtherExpense seeder

---
+++ app/database/seeds/OtherExpenseTableSeeder.php
@@ -0,0 +1,199 @@
+<?php
+
+class ChildTableSeeder extends Seeder{
+
+	public function run(){
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Mobilization';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Demobilization';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Bonds/Ins';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Permits';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Contractors Tax';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Supervision';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'TK/Foreman';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Office Staff';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Office Suppplies';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Transportation Staff';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Communication';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Tools/equipt.';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Barracks';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'temfacil';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'TOILET';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'H. Keeping';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Demolition';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Handling';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Hauling materials';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Hauling out of debris';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Utilities Water';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Utilities Electric';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Temp Util Deposits';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Safety';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Rep';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Bodegero w/ Cellphone';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Professional Fee';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Reports';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Plans';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'As-built plans';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Shop drawing';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Board up';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Signboards';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Signals & warnings';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Sec guard';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Protection of prop';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'exhaust fans/vac';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'ads';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'messenger';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'janitor';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Testing and Commissioning';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Restoration';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Nurse';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Final Cleaning';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Scaffolding';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Line/Grade';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Health Certificate';
+		$otherexp->save();
+
+		$otherexp = new OtherExpense();
+		$otherexp->description = 'Warranty';
+		$otherexp->save();
+	}
+}
