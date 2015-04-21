35b7315407ae76bde3d9e211939f3171b65aaf44
jaotors <turingan.joshua@gmail.com>
2015-04-21 15:06:58 +0800

    added seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 		 $this->call('GroupDatabaseSeeder');
 		 $this->call('UsersTableSeeder');
 		 $this->call('EntryTableSeeder');
+		 $this->call('OtherExpenseTableSeeder');
 	}
 
 }
